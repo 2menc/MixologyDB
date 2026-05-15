4f4847f9dd34d39c10e7dba5ab058b8eaf138532
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-05-15 12:22:00 +0200

    implemnetation of ingredient DAO, new query

--- src/main/java/mix_db/data/dao/Ingredient.java
+++ src/main/java/mix_db/data/dao/Ingredient.java
@@ -3,8 +3,8 @@ package mix_db.data.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 import mix_db.data.dbConnection.DAOException;
 import mix_db.data.dbConnection.DatabaseConnection;
@@ -33,17 +33,50 @@ public class Ingredient {
         return "Ingredient [ingredientName=" + ingredientName + ", numUsed=" + numUsed + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((ingredientName == null) ? 0 : ingredientName.hashCode());
+        result = prime * result + numUsed;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Ingredient other = (Ingredient) obj;
+        if (ingredientName == null) {
+            if (other.ingredientName != null)
+                return false;
+        } else if (!ingredientName.equals(other.ingredientName))
+            return false;
+        if (numUsed != other.numUsed)
+            return false;
+        return true;
+    }
+
     /**
      * DAO obj for Ingredient
      */
     public static final class DAO {
 
+        /**
+         * gets ALL materials (useful for tests)
+         * @param connection the connection
+         * @return a set of Ingredient
+         */
         public static Set<Ingredient> allMaterials(Connection connection) {
-            final Set<Ingredient> allIngredients = new TreeSet<>();
+            final Set<Ingredient> allIngredients = new HashSet<>();
 
             // try with resources: controlla prima che si instanzino correttamente statement e resultSet
             try (
-                final PreparedStatement statement = DatabaseConnection.prepare(connection, Queries.GET_ALL_INGREDIENTS);
+                final PreparedStatement statement = DatabaseConnection.prepare(connection, Queries.ALL_INGREDIENTS);
                 final ResultSet rs = statement.executeQuery();
             ) {
                 while(rs.next()) {
@@ -58,6 +91,26 @@ public class Ingredient {
             }
 
             return allIngredients;
+        }
+
+        public static Set<Ingredient> ofDrink(Connection connection, String drinkID) {
+            final Set<Ingredient> ingredients = new HashSet<>();
+
+            try (
+                final var statement = DatabaseConnection.prepare(connection, Queries.INGREDIENTS_OF_DRINK, drinkID);
+                final var rs = statement.executeQuery();
+            ) {
+                while(rs.next()) {
+                    final String ingredientName = rs.getString("nomeIngrediente");
+                    final int numUsed = rs.getInt("volteUtilizzato");
+                
+                    ingredients.add(new Ingredient(ingredientName, numUsed));
+                }
+            } catch (final Exception e) {
+                throw new DAOException(e);
+            }
+
+            return ingredients;
         }
     }
 }
--- src/main/java/mix_db/data/dbConnection/Queries.java
+++ src/main/java/mix_db/data/dbConnection/Queries.java
@@ -2,10 +2,18 @@ package mix_db.data.dbConnection;
 
 public class Queries {
 
-    public static final String GET_ALL_INGREDIENTS = 
+    public static final String ALL_INGREDIENTS = 
     """
     SELECT nomeIngrediente, volteUtilizzato
     FROM ingredienti
+    """;
+
+    public static final String INGREDIENTS_OF_DRINK =
+    """
+    SELECT I.nomeIngrediente, volteUtilizzato
+    FROM composizioni C, ingredienti I
+    WHERE C.nomeIngrediente = C.nomeIngrediente
+    AND C.drinkID = ?;
     """;
 
 }
