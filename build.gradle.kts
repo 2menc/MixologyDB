bd64c5aae02bf932505d8c55ea9b0f499a36d4af
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-05-15 11:35:37 +0200

    added assertThat library dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.assertj:assertj-core:3.27.3")
+
 }
 
 application {
