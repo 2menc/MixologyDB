5059f2d4916100cd93c19c145a9a608827e03607
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-05-15 11:38:47 +0200

    frst dao implementation - ONLY FULL INGREDIENTS LIST

--- src/main/java/mix_db/data/dao/Ingredient.java
+++ src/main/java/mix_db/data/dao/Ingredient.java
@@ -1,5 +1,15 @@
 package mix_db.data.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import mix_db.data.dbConnection.DAOException;
+import mix_db.data.dbConnection.DatabaseConnection;
+import mix_db.data.dbConnection.Queries;
+
 /**
  * Ingredient
  */
@@ -28,5 +38,26 @@ public class Ingredient {
      */
     public static final class DAO {
 
+        public static Set<Ingredient> allMaterials(Connection connection) {
+            final Set<Ingredient> allIngredients = new TreeSet<>();
+
+            // try with resources: controlla prima che si instanzino correttamente statement e resultSet
+            try (
+                final PreparedStatement statement = DatabaseConnection.prepare(connection, Queries.GET_ALL_INGREDIENTS);
+                final ResultSet rs = statement.executeQuery();
+            ) {
+                while(rs.next()) {
+                    final String ingredientName = rs.getString("nomeIngrediente");
+                    final int numUsed = rs.getInt("volteUtilizzato");
+                    
+                    final Ingredient ingredient = new Ingredient(ingredientName, numUsed);
+                    allIngredients.add(ingredient);
+                }
+            } catch (final Exception e) {
+                throw new DAOException(e);
+            }
+
+            return allIngredients;
+        }
     }
 }
