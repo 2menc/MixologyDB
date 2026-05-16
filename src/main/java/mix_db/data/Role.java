66e65c4a421801464b559fbc7fe2d59b3894c2eb
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-05-16 11:15:50 +0200

    minor changes

---
+++ src/main/java/mix_db/data/Role.java
@@ -0,0 +1,17 @@
+package mix_db.data;
+
+/**
+ * simple role managing 
+ */
+public class Role {
+
+    /**
+     * administrator
+     */
+    public static final String ADMIN = "Admin";
+
+    /**
+     * regolar user
+     */
+    public static final String USER = "User";
+}
