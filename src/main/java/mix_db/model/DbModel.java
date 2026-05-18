81edae90e736cf5bad60c356ed4ddba7eefa94b3
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-05-18 17:04:08 +0200

    created dbModel

---
+++ src/main/java/mix_db/model/DbModel.java
@@ -0,0 +1,140 @@
+package mix_db.model;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+import mix_db.data.dao.Composition;
+import mix_db.data.dao.Drink;
+import mix_db.data.dao.Ingredient;
+import mix_db.data.dao.Review;
+import mix_db.data.dao.Tag;
+import mix_db.data.dao.User;
+
+public class DbModel implements Model{
+
+    private final Connection connection;
+
+    public DbModel(Connection connection) {
+        Objects.requireNonNull(connection, "Null connection");
+        this.connection = connection;
+    }
+
+    @Override
+    public Optional<User> registerUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerUser'");
+    }
+
+    @Override
+    public Optional<User> login(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+    @Override
+    public Optional<Drink> createDrink(Drink drink, Set<Composition> composition, Map<Integer, String> identification) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createDrink'");
+    }
+
+    @Override
+    public Optional<Drink> getDrink(String drinkID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDrink'");
+    }
+
+    @Override
+    public Optional<List<Ingredient>> getIngredients(String drinkID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getIngredients'");
+    }
+
+    @Override
+    public Optional<List<Tag>> getKeywords(String drinkID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getKeywords'");
+    }
+
+    @Override
+    public boolean saveAsFavourite(String drinkID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveAsFavourite'");
+    }
+
+    @Override
+    public List<Drink> getFavourites() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFavourites'");
+    }
+
+    @Override
+    public boolean addReview(String drinkID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addReview'");
+    }
+
+    @Override
+    public Optional<Drink> searchForKeywords(String keyword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchForKeywords'");
+    }
+
+    @Override
+    public Map<Drink, Integer> calculateDrinkBestReviewsLeaderboard(int daysAgo, int numberOfResults) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateDrinkBestReviewsLeaderboard'");
+    }
+
+    @Override
+    public List<Ingredient> getMostUsedIngredients(int numberOfResults) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMostUsedIngredients'");
+    }
+
+    @Override
+    public List<User> calculateUsersWithMostPositiveReviewsLeaderboard(int numberOfResults) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateUsersWithMostPositiveReviewsLeaderboard'");
+    }
+
+    @Override
+    public Map<String, Integer> getTrendingKeywords(int daysAgo, int numberOfResults) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTrendingKeywords'");
+    }
+
+    @Override
+    public List<Drink> getSuggestions(int numberOfResults) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSuggestions'");
+    }
+
+    @Override
+    public Map<User, List<Review>> getUserAnalitics() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserAnalitics'");
+    }
+
+    @Override
+    public boolean deleteReview(String userID, String drinkID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteReview'");
+    }
+
+    @Override
+    public boolean deleteDrink(String drinkID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteDrink'");
+    }
+
+    @Override
+    public boolean banUser(String userID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'banUser'");
+    }
+
+}
