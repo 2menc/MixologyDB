a4b1e4fc443753fea66bbe54216366fadba5a0b4
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-05-30 19:39:33 +0200

    added sql population script

---
+++ popolazione.sql
@@ -0,0 +1,6 @@
+INSERT INTO Categorie (nomeCategoria, descrizione) VALUES
+('The Unforgettables', 'I grandi classici della mixology.'),
+('Contemporary Classics', 'Drink classici ma di epoca moderna.'),
+('New Era Drinks', 'I cocktail innovativi e le tendenze moderne.'),
+('Tiki', 'Cocktail esotici a base di frutta e rum.'),
+('Mocktail', 'Drink analcolici per chi non beve alcol.');
