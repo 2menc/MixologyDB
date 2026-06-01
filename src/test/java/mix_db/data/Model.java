4eb9ed5ebe5e0e3e9e444e0fcfa34d5d0670a01f
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-06-01 21:08:19 +0200

    removed useless imports

--- src/test/java/mix_db/data/Model.java
+++ src/test/java/mix_db/data/Model.java
@@ -1,16 +1,9 @@
 package mix_db.data;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
 
 import java.sql.Connection;
 import java.sql.Date;
